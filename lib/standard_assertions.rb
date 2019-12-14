ddfab1730d82dee9ca8905b1476efffabbc5a19c
yasaichi <yasaichi@users.noreply.github.com>
2019-12-14 11:38:38 +0900

    Initial commit

---
+++ lib/standard_assertions.rb
@@ -0,0 +1,6 @@
+require "standard_assertions/version"
+
+module StandardAssertions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/standard_assertions/version.rb
@@ -0,0 +1,3 @@
+module StandardAssertions
+  VERSION = "0.1.0"
+end
